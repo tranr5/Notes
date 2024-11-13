f531ad3b968c1b4554b742d5b6387c2d0891df91
tranr5 <tranr4@gmail.com>
2024-11-13 15:34:47 -0500

    1st commit

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 // touch file= make new file
 // code file = open file
 // rm file = remove file   rmdir file = remove folder
+// at folder I can type "code ."
 
 // Visual Studio Code shortcuts
 // control + Backtick (`) = brings up terminal
@@ -11,4 +12,32 @@
 // git commit -m "notess"
 // 
 
+// == : Equal to (loose equality)
+// === : Strict equal to (checks both value and type)
+// != : Not equal to (loose inequality)
+// !== : Strict not equal to
+// > : Greater than
+// < : Less than
+// >= : Greater than or equal to
+// <= : Less than or equal to
+// 4. Logical Operators:
+// && : Logical AND
+// || : Logical OR
+// ! : Logical NOT
+console.log('science'.indexOf('x') === -1)
 
+
+
+
+
+21 % 2 === 1
+12 % 3 === 0
+9 % 3 === 0
+14 % 3 === 0
+
+'new york'[1]
+
+
+
+'engineering'.indexOf('neer')
+'engineering'.indexOf('r') > -1
