85d940d7853740675e5a97f84d0b23f0be453c5c
tranr5 <tranr4@gmail.com>
2024-11-13 14:07:11 -0500

    https://github.com/tranr5/notes.git

---
+++ main.js
@@ -0,0 +1,14 @@
+// terminal codes
+// ls cd mkdir file=make new folder
+// touch file= make new file
+// code file = open file
+// rm file = remove file   rmdir file = remove folder
+
+// Visual Studio Code shortcuts
+// control + Backtick (`) = brings up terminal
+// 
+// git add .
+// 
+// 
+
+
