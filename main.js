6db3e8c26e9d28cb5b8fb7cc1d1bfc73e9801292
tranr5 <tranr4@gmail.com>
2024-11-19 09:38:58 -0500

    functions 2

--- main.js
+++ main.js
@@ -83,21 +83,99 @@
 // console.log(checkSquare())
 
 
-  const checkSquare = (num) => {
-    const squareRoot = Math.sqrt(num)
-    
-    if (squareRoot % 1===0) {
-        return squareRoot
-    } else {
-        return "not a perfect square"
-    }
-    
-  }
 
-const checkNum = (limit) => {
-    // for int = ""
-    for (let i = 1; i <= limit; i++) {
-        console.log(checkSquare(i))
-    }
+// checks if perfect square alll the way to the num i picked
+//   const checkSquare = (num) => {
+//     const squareRoot = Math.sqrt(num)
+    
+//     if (squareRoot % 1===0) {
+//         return squareRoot
+//     } else {
+//         return "not a perfect square"
+//     }
+    
+//   }
+
+// const checkNum = (limit) => {
+//     // for int = ""
+//     for (let i = 1; i <= limit; i++) {
+//         console.log(checkSquare(i))
+//     }
+// }
+// checkNum(100)
+
+
+
+
+
+// 1.1. What is the difference between a parameter and an argument?
+
+// 1.2. Within a function, what is the difference between return and console.log?
+
+// 1. 3. What are the implications of the ability of a function to return a value?
+
+// 2. calculateCube
+// function calculateCube(num) {
+//     num = num * num * num
+//     return num
+// }
+// console.log(calculateCube(3))
+// 3. isAVowel
+// function isAVowel(letter) {
+//     return['a','A','e','E','i','I','o','O','u','U'].includes(letter)
+// }
+// console.log(isAVowel('b'))
+// 4. getTwoLengths
+function getTwoLengths(word1, word2) {
+    return [word1.length,word2.length]
 }
-checkNum(100)
+console.log(getTwoLengths("dog","pizza"))
+// 5. sumArray
+function sumArray(arr) {
+    // YOUR CODE HERE
+}
+
+// 6.1 checkPrime
+function checkPrime(num) {
+    // YOUR CODE HERE
+}
+
+// 6.2 printPrimes
+function printPrimes(num) {
+    // YOUR CODE HERE
+}
+
+// 7. printLongestWord
+function printLongestWord(arr) {
+    // YOUR CODE HERE
+}
+
+// BONUS!
+
+// 8. eulerFibo
+function eulerFibo(num) {
+    // YOUR CODE HERE
+}
+
+// 9. findNeedle
+function findNeedle(arr) {
+    // YOUR CODE HERE
+}
+
+// 10. sumPositive
+function sumPositive(arr) {
+    // YOUR CODE HERE
+}
+
+// module.exports = {
+//     calculateCube,
+//     isAVowel,
+//     getTwoLengths,
+//     sumArray,
+//     checkPrime,
+//     printPrimes,
+//     printLongestWord,
+//     eulerFibo,
+//     findNeedle,
+//     sumPositive
+// };
